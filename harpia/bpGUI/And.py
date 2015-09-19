e489c8c95c948de34ff0655b7a4e47bb7a4d7ae8
exterminus <cmagnobarbosa@gmail.com>
2015-09-18 21:05:56 -0300

    Atualização

---
+++ harpia/bpGUI/And.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# [HARPIA PROJECT]
+#
+#
+# S2i - Intelligent Industrial Systems
+# DAS - Automation and Systems Department
+# UFSC - Federal University of Santa Catarina
+# Copyright: 2006 - 2007 Luis Carlos Dill Junges (lcdjunges@yahoo.com.br), Clovis Peruchi Scotti (scotti@ieee.org),
+#                        Guilherme Augusto Rutzen (rutzen@das.ufsc.br), Mathias Erdtmann (erdtmann@gmail.com) and S2i (www.s2i.das.ufsc.br)
+#            2007 - 2009 Clovis Peruchi Scotti (scotti@ieee.org), S2i (www.s2i.das.ufsc.br)
+#
+#
+#    This program is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU General Public License version 3, as published
+#    by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranties of
+#    MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#    PURPOSE.  See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    For further information, check the COPYING file distributed with this software.
+#
+# ----------------------------------------------------------------------
+
+from harpia.GladeWindow import GladeWindow
+import gtk
+from harpia.s2icommonproperties import S2iCommonProperties, APP, DIR
+import os
+# i18n
+import gettext
+
+_ = gettext.gettext
+gettext.bindtextdomain(APP, DIR)
+gettext.textdomain(APP)
+
+
+# ----------------------------------------------------------------------
+
+class Properties(GladeWindow, S2iCommonProperties):
+    # ----------------------------------------------------------------------
+
+    def __init__(self, PropertiesXML, S2iBlockProperties):
+        self.m_sDataDir = os.environ['HARPIA_DATA_DIR']
+
+        filename = self.m_sDataDir + 'glade/and.ui'
+        self.m_oPropertiesXML = PropertiesXML
+        self.m_oS2iBlockProperties = S2iBlockProperties
+
+        widget_list = [
+            'Properties',
+            'BackgroundColor',
+            'BorderColor',
+            'HelpView'
+        ]
+
+        handlers = [
+            'on_BackColorButton_clicked',
+            'on_BorderColorButton_clicked',
+            'on_cancel_clicked',
+            'on_and_confirm_clicked'
+        ]
+
+        top_window = 'Properties'
+
+        GladeWindow.__init__(self, filename, top_window, widget_list, handlers)
+
+        self.configure()
+
+    # ----------------------------------------------------------------------
+
+    def __del__(self):
+        pass
+
+    # ----------------------------------------------------------------------
+
+    def on_and_confirm_clicked(self, *args):
+        self.m_oS2iBlockProperties.SetBorderColor(self.m_oBorderColor)
+        self.m_oS2iBlockProperties.SetBackColor(self.m_oBackColor)
+        self.widgets['Properties'].destroy()
+
+        # ----------------------------------------------------------------------
+
+    def getHelp(self):
+        return "Permite a operação lógica 'E' entre as duas entradas. Para esse bloco há duas possibilidades. Primeira: Executa a operação entre duas imagens ponto a ponto. Segunda: Executa a operação entre um valor constante e cada ponto da imagem."
+
+
+# ------------------------------------------------------------------------------
+# Code generation
+# ------------------------------------------------------------------------------
+def generate(blockTemplate):
+    import harpia.gerador
+    blockTemplate.imagesIO = \
+        'IplImage * block' + blockTemplate.blockNumber + '_img_i1 = NULL;\n' + \
+        'IplImage * block' + blockTemplate.blockNumber + '_img_i2 = NULL;\n' + \
+        'IplImage * block' + blockTemplate.blockNumber + '_img_o1 = NULL;\n'
+    blockTemplate.functionCall = '\nif(block' + blockTemplate.blockNumber + '_img_i1){\n' + \
+                                 'block' + blockTemplate.blockNumber + '_img_o1 = cvCreateImage(cvSize(block' + blockTemplate.blockNumber + \
+                                 '_img_i1->width,block' + blockTemplate.blockNumber + '_img_i1->height),block' + blockTemplate.blockNumber + \
+                                 '_img_i1->depth,block' + blockTemplate.blockNumber + '_img_i1->nChannels);\n' + \
+                                 harpia.gerador.inputSizeComply(2, blockTemplate.blockNumber) + 'cvAnd(block' + \
+                                 blockTemplate.blockNumber + '_img_i1, block' + blockTemplate.blockNumber + '_img_i2, block' + \
+                                 blockTemplate.blockNumber + '_img_o1,0);\n cvResetImageROI(block' + blockTemplate.blockNumber + '_img_o1);}\n'
+    blockTemplate.dealloc = 'cvReleaseImage(&block' + blockTemplate.blockNumber + '_img_o1);\n' + \
+                            'cvReleaseImage(&block' + blockTemplate.blockNumber + '_img_i1);\n' + \
+                            'cvReleaseImage(&block' + blockTemplate.blockNumber + '_img_i2);\n'
+
+
+# ------------------------------------------------------------------------------
+# Block Setup
+# ------------------------------------------------------------------------------
+def getBlock():
+    return {"Label": _("And"),
+            "Path": {"Python": "And",
+                     "Glade": "glade/and.ui",
+                     "Xml": "xml/and.xml"},
+            "Inputs": 2,
+            "Outputs": 1,
+            "Icon": "images/and.png",
+            "Color": "10:180:10:150",
+            "InTypes": {0: "HRP_IMAGE", 1: "HRP_IMAGE"},
+            "OutTypes": {0: "HRP_IMAGE"},
+            "Description": _("Logical AND operation between two images."),
+            "TreeGroup": _("Arithmetic and logical operations")
+            }
