95bcca56a1761ef535b103aa583bff32f67262e9
llgoncalves <luanlg.cco@gmail.com>
2016-08-24 22:58:03 -0300

    Correção no botão 'Ok' na janela de preferências. Adicionado o botão 'browser', falta adicionar a ação.

--- harpia/GUI/harpiapropertywindow.py
+++ harpia/GUI/harpiapropertywindow.py
@@ -15,7 +15,7 @@ class HarpiaPropertyWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="Properties")
         self.properties = properties
         self.set_parent(main_window)
-
+        self.set_border_width(10)
         vbox = Gtk.VBox()
         self.add(vbox)
 
@@ -27,6 +27,10 @@ class HarpiaPropertyWindow(Gtk.Window):
         self.default_directory = Gtk.Entry()
         self.default_directory.set_text(self.properties.get_default_directory())
         hbox.add(self.default_directory)
+
+        browser = Gtk.Button.new_with_label("Browser")
+        hbox.pack_start(browser, False, False, 0)
+        browser.connect("clicked", self.__on_browser_clicked)
 
         # Error Log File
         hbox = Gtk.HBox()
@@ -40,14 +44,16 @@ class HarpiaPropertyWindow(Gtk.Window):
         hbox = Gtk.HBox()
         vbox.add(hbox)
         self.ok_button = Gtk.Button("Ok")
-        self.ok_button.connect("key-press-event", self.__on_key_press)
+        self.ok_button.connect("clicked", self.__on_ok_clicked)
         hbox.add(self.ok_button)
 
         self.show_all()
 
     #----------------------------------------------------------------------
-    def __on_key_press(self, widget, event=None):
+    def __on_ok_clicked(self, widget, event=None):
         self.properties.set_error_log_file(self.error_log_file.get_text())
         self.properties.set_default_directory(self.default_directory.get_text())
         self.close()
 #----------------------------------------------------------------------
+    def __on_browser_clicked(self, widget):
+        pass
