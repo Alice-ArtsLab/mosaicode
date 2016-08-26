cae22baaf9f1f8cef13fc26aab289298f919e26f
Flavio Schiavoni <fls@rendera.com.br>
2016-08-26 19:24:24 -0300

    Added time and date, text format and auto scroll

--- harpia/GUI/status.py
+++ harpia/GUI/status.py
@@ -4,6 +4,9 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from gi.repository import Pango
+
+import datetime
 
 class Status(Gtk.ScrolledWindow):
 
@@ -17,13 +20,25 @@ class Status(Gtk.ScrolledWindow):
         self.status.set_editable(False)
         self.status.set_left_margin(2)
         self.add(self.status)
+        textbuffer = self.status.get_buffer()
+        self.tag_bold = textbuffer.create_tag("bold", weight=Pango.Weight.BOLD)
+        self.tag = textbuffer.create_tag("red", weight=Pango.Weight.NORMAL)
+        self.tag_red = textbuffer.create_tag("normal", background="red")
+
 
     def clear(self):
         self.status.get_buffer().set_text("")
 
     def append_text(self, text):
-        text = "Data e hora " + text + "\n"
-        self.status.get_buffer().insert_at_cursor(text, len(text))
+        textbuffer = self.status.get_buffer()
+        end_iter = textbuffer.get_end_iter()
+        msg = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        textbuffer.insert_with_tags(end_iter, msg, self.tag_bold)
+
+        end_iter = textbuffer.get_end_iter()
+        msg = " - " + text + "\n"
+        textbuffer.insert_with_tags(end_iter, msg, self.tag)
+        self.status.scroll_to_mark(textbuffer.get_insert(), 0.0, True, 0.5, 0.5)
 
     def log(self, text):
         self.append_text(text)
