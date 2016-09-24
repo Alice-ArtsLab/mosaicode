5ec593d29cbdebe74ef0eb516b4911a2ed2ec3ea
Flavio Schiavoni <fls@rendera.com.br>
2016-09-23 22:23:13 -0300

    New webaudio plugin trigger a float from a char

---
+++ harpia/plugins/javascript/webaudio/char2float.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+from harpia.constants import *
+import gettext
+_ = gettext.gettext
+gettext.bindtextdomain(APP, DIR)
+gettext.textdomain(APP)
+
+from harpia.GUI.fieldtypes import *
+from harpia.model.plugin import Plugin
+
+class Char2Float(Plugin):
+
+# ------------------------------------------------------------------------------
+    def __init__(self):
+        Plugin.__init__(self)
+        self.id = -1
+        self.type = self.__class__.__module__
+        self.float = 60
+        self.char = 'a'
+
+    # ----------------------------------------------------------------------
+    def get_help(self):#Função que chama a help
+        return "char to float"
+
+    # ----------------------------------------------------------------------
+    def generate_header(self):
+        return ""
+
+    # ----------------------------------------------------------------------
+    def generate_vars(self):
+        return """
+var block_$id$_o1 = [];
+var block_$id$_i = [];
+block_$id$_i[1] = function(value){
+    if (value != '$char$')
+        return true;
+    for (var i = 0; i < block_$id$_o1.length ; i++){
+        block_$id$_o1[i]($float$);
+    }
+    return true;
+    };
+"""
+
+    # ----------------------------------------------------------------------
+    def generate_function_call(self):
+        return """
+"""
+
+    # ----------------------------------------------------------------------
+    def generate_dealloc(self):
+        return """"""
+
+
+    # ----------------------------------------------------------------------
+    def generate_out_dealloc(self):
+        return ""
+
+    # ----------------------------------------------------------------------
+    def __del__(self):
+        pass
+
+    # ----------------------------------------------------------------------
+    def get_description(self):
+        return {"Type": str(self.type),
+            "Label": _("Char 2 Float"),
+            "Icon": "images/show.png",
+            "Color": "200:200:25:150",
+            "InTypes": {0: "HRP_WEBAUDIO_CHAR"},
+            "OutTypes": {0: "HRP_WEBAUDIO_FLOAT"},
+            "Description": _("Char 2 Float"),
+            "TreeGroup": _("Conversion")
+            }
+
+    # ----------------------------------------------------------------------
+    def get_properties(self):
+        return {"float":{"name": "Output float",
+                    "type": HARPIA_FLOAT,
+                    "value": self.float,
+                    "lower":0,
+                    "upper":20000,
+                    "step":1
+                    },
+                "char":{"name": "Input Char",
+                    "type": HARPIA_STRING,
+                    "value": self.char
+                    }
+        }
+
+# ------------------------------------------------------------------------------
