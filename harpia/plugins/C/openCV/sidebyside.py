53aa6ee99e06533ca519ec308eecaabfd9022bfd
Flavio Schiavoni <fls@rendera.com.br>
2016-10-01 01:14:32 -0300

    New plugins to display image side by side

---
+++ harpia/plugins/C/openCV/sidebyside.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+from harpia.GUI.fieldtypes import *
+from harpia.plugins.C.openCV.opencvplugin import OpenCVPlugin
+
+class SideBySide(OpenCVPlugin):
+
+# ------------------------------------------------------------------------------
+    def __init__(self):
+        OpenCVPlugin.__init__(self)
+
+    # --------------------------Help Text--------------------------------------------
+    def get_help(self):#Função que chama a help
+        return "Coloca uma imagem do lado da outra"
+
+    # ----------------------------------------------------------------------
+    def generate_function_call(self):
+        return \
+            'if(block$id$_img_i0 && block$id$_img_i1){\n' + \
+            'int width=block$id$_img_i0->width + block$id$_img_i1->width;\n' + \
+            'int height= (block$id$_img_i0->height > block$id$_img_i1->height)?block$id$_img_i0->height:block$id$_img_i1->height;\n' + \
+            'block$id$_img_o0=cvCreateImage(cvSize(width,height),IPL_DEPTH_8U,3); \n' + \
+            'cvSetImageROI(block$id$_img_o0, cvRect(0, 0, block$id$_img_i0->width, block$id$_img_i0->height) );\n' + \
+            'cvCopy(block$id$_img_i0,block$id$_img_o0,NULL);\n' + \
+            'cvResetImageROI(block$id$_img_o0);\n' + \
+            'cvSetImageROI(block$id$_img_o0, cvRect(block$id$_img_i0->width, 0, width, block$id$_img_i1->height) );\n' + \
+            'cvCopy(block$id$_img_i1,block$id$_img_o0,NULL);\n' + \
+            'cvResetImageROI(block$id$_img_o0);\n' + \
+            '}\n'
+
+    # ----------------------------------------------------------------------
+    def generate_dealloc(self):
+        return 'if (block$id$_img_o0) cvReleaseImage(&block$id$_img_o0);\n' + \
+            'cvReleaseImage(&block$id$_img_i0);\n'+ \
+            'cvReleaseImage(&block$id$_img_i1);\n'
+
+    # ------------------------------------------------------------------------------
+    def get_description(self):
+        return {"Label": "Side By Side",
+                "Icon": "images/and.png",
+                "Color": "10:180:10:150",
+                "InTypes": {0: "HRP_IMAGE", 1: "HRP_IMAGE"},
+                "OutTypes": {0: "HRP_IMAGE"},
+                "TreeGroup": "Arithmetic and logical operations"
+                }
+
+    # ------------------------------------------------------------------------------
+    def get_properties(self):
+        return {}
+
+# ------------------------------------------------------------------------------
