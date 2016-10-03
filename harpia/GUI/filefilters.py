767c8f4bc3f5aa475128be75ab452ac510404306
Flavio Schiavoni <fls@rendera.com.br>
2016-10-03 12:46:57 -0300

    Removed unused filefilters

--- harpia/GUI/filefilters.py
+++ harpia/GUI/filefilters.py
@@ -15,52 +15,8 @@ class HarpiaFileFilter(Gtk.FileFilter):
         self.set_name("Harpia Files")
         self.add_pattern("*.hrp")
 
-class CCOdeFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("C Code (*.c)")
-        self.add_pattern("*.c")
-
 class PNGFileFilter(Gtk.FileFilter):
     def __init__(self):
         Gtk.FileFilter.__init__(self)
         self.set_name("Png")
         self.add_pattern("*.png")
-
-class JPGFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("JPG")
-        self.add_pattern("*.jpg")
-
-class XMLImageFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("XML Image")
-        self.add_pattern("*.xml")
-
-
-class VideoFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("Videos")
-        self.add_mime_type("*.mpeg")
-        self.add_mime_type("*.avi")
-        self.add_mime_type("*.mpg")
-        self.add_mime_type("*.wmv")
-
-class ImageFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("Images")
-        self.add_mime_type("*.jpg")
-        self.add_mime_type("*.bmp")
-        self.add_mime_type("*.png")
-        self.add_mime_type("*.gif")
-
-class AVIFileFilter(Gtk.FileFilter):
-    def __init__(self):
-        Gtk.FileFilter.__init__(self)
-        self.set_name("AVI Videos")
-        self.add_mime_type("*.avi")
-
