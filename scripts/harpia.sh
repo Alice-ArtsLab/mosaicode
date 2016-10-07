cb9fe9f1a0b3eb66a3ee92e45bb9d74b2fc3265c
Carlos Magno <cmagnobarbosa@gmail.com>
2016-10-07 13:22:34 -0300

    ajustes para o pip

--- scripts/harpia.sh
+++ scripts/harpia.sh
@@ -1,7 +1,7 @@
+echo "Resolvendo problema de dependências"
 sudo apt-get install libopencv-dev python-opencv
 sudo apt-get install python-gtk2 python-glade2
 sudo apt-get install python-gnome2
 sudo apt-get install libgoocanvas-2.0-common
 sudo apt-get install gir1.2-goocanvas-2.0
 sudo apt-get install python-lxml
-sudo apt install harpia
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-DISTUTILS_DEBUG="True"
+DISTUTILS_DEBUG = "True"
 
 from glob import glob
 
@@ -11,46 +11,54 @@ except ImportError:
 config = {}
 
 config['classifiers'] = [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: C',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Software Development :: Code Generators',
-            ]
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: C',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Code Generators',
+]
 
 setup(name='harpia',
-            install_requires=['beautifulsoup4','pip'],
-            version='1.0',
-            packages=['harpia',
-                      'harpia.utils',
-                      'harpia.GUI',
-                      'harpia.GUI.components',
-                      'harpia.plugins',
-                      'harpia.plugins.C',
-                      'harpia.plugins.C.openCV',
-                      'harpia.plugins.javascript',
-                      'harpia.plugins.javascript.webaudio',
-                      'harpia.control',
-                      'harpia.model'],
-            scripts=['launcher/harpia','scripts/req.sh'],
-            description='Image Processing and Computer Vision Automatic Programming Tool',
-            author='Clovis Peruchi Scotti',
-            author_email='scotti@ieee.org',
-            maintainer="Clovis Peruchi Scotti",
-            maintainer_email="scotti@ieee.org",
-            license="GNU GPL",
-            url='http://s2i.das.ufsc.br/harpia/en/home.html',
-            data_files=[ ('/usr/share/harpia/images', glob("app_data/images/*")), #this is fucked up! must put it in package_data!!
-                         ('/usr/share/harpia/po/pt/LC_MESSAGES/', glob("app_data/po/pt/LC_MESSAGES/*")),
-                         ('/usr/share/harpia/examples', glob("app_data/examples/*.hrp")),
-                         ('/usr/share/applications/', ["app_data/harpia.desktop"]),
-                        ('/usr/share/icons/hicolor/scalable/apps', ['app_data/images/harpia.svg']),
-                        ('/usr/share/pixmaps', ['app_data/images/harpia.svg']),
-                        ('/usr/share/icons/hicolor/24x24/apps', ['app_data/images/harpia.png']),],
-            **config
-            )
+      install_requires=['beautifulsoup4', 'pip'],
+      version='1.0a1',
+      packages=[
+          'app_data',
+          'harpia',
+          'harpia.utils',
+          'harpia.GUI',
+          'harpia.GUI.components',
+          'harpia.plugins',
+          'harpia.plugins.C',
+          'harpia.plugins.C.openCV',
+          'harpia.plugins.javascript',
+          'harpia.plugins.javascript.webaudio',
+          'harpia.control',
+          'harpia.model'],
+      scripts=['launcher/harpia', 'scripts/req.sh'],
+      description='Image Processing and Computer Vision Automatic Programming Tool',
+      author='Ouroboros',
+      author_email='cmagnobarbosa+harpia@gmail.com',
+      maintainer="Ourobos",
+      maintainer_email="cmagnobarbosa+harpia@gmail.com",
+      license="GNU GPL",
+      url='http://ges.dcomp.ufsj.edu.br/index.php/ouroboros/',
+
+      data_files=[('/usr/share/harpia/images', glob("app_data/images/*")),  # this is fucked up! must put it in package_data!!
+                  ('/usr/share/harpia/po/pt/LC_MESSAGES/',
+                   glob("app_data/po/pt/LC_MESSAGES/*")),
+                  ('/usr/share/harpia/examples',
+                   glob("app_data/examples/*.hrp")),
+                  ('/usr/share/applications/',
+                   ["app_data/harpia.desktop"]),
+                  ('/usr/share/icons/hicolor/scalable/apps',
+                   ['app_data/images/harpia.svg']),
+                  ('/usr/share/pixmaps',
+                   ['app_data/images/harpia.svg']),
+                  ('/usr/share/icons/hicolor/24x24/apps', ['app_data/images/harpia.png']), ],
+      **config
+      )
