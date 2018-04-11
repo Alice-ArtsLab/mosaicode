b02bd0769533ea9b2cdf57f2ea91aeae854d2267
Unknown <jmsandy@gmail.com>
2018-04-10 23:28:59 -0300

    Modification the path to code generation and the listening to web server.

--- mosaicode/model/preferences.py
+++ mosaicode/model/preferences.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
+
 
 class Preferences(object):
+    """
+    This class contain the default preferences from mosaicode user.
+    """
 
     def __init__(self):
         self.conf_file_path = "configuration.xml"
         self.recent_files = []
-        self.default_directory = "/tmp/%l/%n-%t"
+        # self.default_directory = "/tmp/%l/%n-%t"
+        from mosaicode.system import System
+        self.default_directory = os.path.join(System.get_user_dir(), "code-gen")
         self.default_filename = "%n"
         self.grid = 10
         self.port = 49151
