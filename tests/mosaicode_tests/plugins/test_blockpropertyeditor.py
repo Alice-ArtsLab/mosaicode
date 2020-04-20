fc6f1ecdfee7fdccc9835355402f58b51c48def4
Flavio Schiavoni <fls@rendera.com.br>
2020-04-20 19:10:14 -0300

    moved test to the correct folder

--- tests/mosaicode_tests/plugins/test_blockpropertyeditor.py
+++
@@ -1,9 +0,0 @@
-from tests.mosaicode_tests.test_base import TestBase
-from mosaicode.plugins.extensionsmanager.GUI.blockpropertyeditor \
-    import BlockPropertyEditor
-
-
-class TestBlockPropertyEditor(TestBase):
-
-    def setUp(self):
-        self.blockpropertyeditor = BlockPropertyEditor(None, None)
